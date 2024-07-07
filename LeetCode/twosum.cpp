987d2b091c9a3d7017affe2112b1dabff363c0e7
NathanKurths <nathankurthsm@gmail.com>
2024-07-07 18:45:39 -0300

    att

---
+++ LeetCode/twosum.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <vector>
+
+class Solution {
+public:
+    std::vector<int> twoSum(std::vector<int>& nums, int target) {
+        nums = 
+        int target = numbers[0] + numbers[1];
+        
+    }
+};
